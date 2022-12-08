5d1b0731a8bfb9c1a65e212b4bd01cb8a7ca51ec
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-08 21:05:20 +0100

    fixed

--- src/immigrantsCollection/ImmigrantsCollection.java
+++ src/immigrantsCollection/ImmigrantsCollection.java
@@ -1,5 +1,20 @@
 package immigrantsCollection;
 
+import immigrants.Immigrant;
+import immigrants.ImmigrantFactory;
+import immigrants.ImmigrantType;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImmigrantsCollection
 {
+    public void registerImmigrant(String lastName, Double x, Double y, String firstName)
+    {
+        ImmigrantType type = ImmigrantFactory.getImmigrantType(firstName);
+        Immigrant immigrant = new Immigrant(lastName, x, y, type);
+        immigrants.add(immigrant);
+    }
+
+    private List<Immigrant> immigrants = new ArrayList<>();
 }
