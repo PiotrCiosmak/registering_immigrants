b942864ce4b35aa666a0234023754646979ec276
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-08 20:38:48 +0100

    constructor implemented show function implemented
    x, y, type: private var added

--- src/immigrants/Immigrant.java
+++ src/immigrants/Immigrant.java
@@ -2,5 +2,19 @@ package immigrants;
 
 public class Immigrant
 {
+    public Immigrant(Double x, Double y, ImmigrantType type)
+    {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
 
+    public void show()
+    {
+        type.show(x, y);
+    }
+
+    private Double x;
+    private Double y;
+    private ImmigrantType type;
 }
