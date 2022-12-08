de92336afd590bfbef7315573d263df6160913df
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-08 20:59:51 +0100

    fixed

--- src/immigrants/Immigrant.java
+++ src/immigrants/Immigrant.java
@@ -2,19 +2,21 @@ package immigrants;
 
 public class Immigrant
 {
-    public Immigrant(Double x, Double y, ImmigrantType type)
+    public Immigrant(Double x, Double y, String lastName, ImmigrantType type)
     {
         this.x = x;
         this.y = y;
+        this.lastName = lastName;
         this.type = type;
     }
 
     public void show()
     {
-        type.show(x, y);
+        type.show(x, y, lastName);
     }
 
     private Double x;
     private Double y;
+    private String lastName;
     private ImmigrantType type;
 }
