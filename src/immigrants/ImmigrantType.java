ce1d9a9485835e240ca400496a7c5e7aa2ad857c
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-08 20:39:17 +0100

    constructor implemented show function implemented
    firstName, lastName: private var added

--- src/immigrants/ImmigrantType.java
+++ src/immigrants/ImmigrantType.java
@@ -2,4 +2,20 @@ package immigrants;
 
 public class ImmigrantType
 {
+    public ImmigrantType(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void show(Double x, Double y)
+    {
+        System.out.println("---IMIGRANT---");
+        System.out.println("Imie: " + firstName);
+        System.out.println("Nazwisko: " + lastName);
+        System.out.println("Współrzędne: [" + x + ", " + y + "]\n");
+    }
+
+    private String firstName;
+    private String lastName;
 }
