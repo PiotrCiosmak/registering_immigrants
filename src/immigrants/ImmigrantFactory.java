9dc2420a80aca89531d04e695568f7a34949fae6
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-08 20:46:39 +0100

    getImmigrantType function implemented
    immigrantTypes: private var added

--- src/immigrants/ImmigrantFactory.java
+++ src/immigrants/ImmigrantFactory.java
@@ -1,5 +1,20 @@
 package immigrants;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ImmigrantFactory
 {
+    public static ImmigrantType getImmigrantType(String firstName, String lastName)
+    {
+        ImmigrantType result = immigrantTypes.get(firstName + lastName);
+        if (result == null)
+        {
+            result = new ImmigrantType(firstName, lastName);
+            immigrantTypes.put(firstName + lastName, result);
+        }
+        return result;
+    }
+
+    static Map<String, ImmigrantType> immigrantTypes = new HashMap<>();
 }
