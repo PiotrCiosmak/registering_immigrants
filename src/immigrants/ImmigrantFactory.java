eca445303a42c57b27618879dec36edd406b0621
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-08 21:03:04 +0100

    fixed

--- src/immigrants/ImmigrantFactory.java
+++ src/immigrants/ImmigrantFactory.java
@@ -5,13 +5,13 @@ import java.util.Map;
 
 public class ImmigrantFactory
 {
-    public static ImmigrantType getImmigrantType(String firstName, String lastName)
+    public static ImmigrantType getImmigrantType(String firstName)
     {
-        ImmigrantType result = immigrantTypes.get(firstName + lastName);
+        ImmigrantType result = immigrantTypes.get(firstName);
         if (result == null)
         {
-            result = new ImmigrantType(firstName, lastName);
-            immigrantTypes.put(firstName + lastName, result);
+            result = new ImmigrantType(firstName);
+            immigrantTypes.put(firstName, result);
         }
         return result;
     }
